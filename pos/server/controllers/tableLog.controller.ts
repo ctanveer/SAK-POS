6a09e101c91ec7ecf5b7f87cf409437c62290e8d
Tanveer Chowdhury <ct.anzoom@gmail.com>
2024-01-15 17:10:09 +0600

    added route for update tableLog by Id

--- pos/server/controllers/tableLog.controller.ts
+++ pos/server/controllers/tableLog.controller.ts
@@ -31,6 +31,18 @@ export const getTableLogsByTableIdController = async (req: Request, res: Respons
     }
 };
 
+export const updateTableLogByIdController = async (req: Request, res: Response) => {
+  try {
+    const tableLogId = req.params.id;
+    const tableLogObject = { ...req.body };
+    const tableLog = await updateTableLogById(tableLogId, tableLogObject);
+    res.json(tableLog);
+  } catch (error: any) {
+    res.status(500);
+    res.json({ error: error.message });
+  }
+};
+
 export const createTableLogController = async (req: Request, res: Response) => {
     try {
         const tableObject = {...req.body };
