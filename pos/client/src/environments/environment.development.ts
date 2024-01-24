8c9d79b5b3c547ddfe6a578683c4cd947df21a3b
Tanveer Chowdhury <ct.anzoom@gmail.com>
2024-01-24 21:14:14 +0600

    changed to render backend

--- pos/client/src/environments/environment.development.ts
+++ pos/client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  API_URL: 'http://localhost:3000'
+  // API_URL: 'http://localhost:3000'
   // API_URL: 'https://bento-pos-tanveer-c.koyeb.app'
+  API_URL: 'https://bento-pos-server.onrender.com'
 };
--- pos/client/src/environments/environment.ts
+++ pos/client/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  API_URL: 'https://bento-pos-tanveer-c.koyeb.app'
+  // API_URL: 'https://bento-pos-tanveer-c.koyeb.app'
+  API_URL: 'https://bento-pos-server.onrender.com'
 };
